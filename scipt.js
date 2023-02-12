c7c110458df8152cdd55bbc74dc9fa26c030cab6
Jonathan Patino <johnypatino99@gmail.com>
2023-02-12 15:38:37 -0500

    Added Ui

--- scipt.js
+++ scipt.js
@@ -1,67 +1,91 @@
+const results = document.querySelector('.results');
+const content = document.createElement('p');
+const scores = document.createElement('p')
+const winnerResult = document.createElement('p');
+let playerScore = 0;
+let computerScore = 0;
+const paper = document.getElementById('paper');
+paper.addEventListener('click', () => {
+    content.textContent = (playGame('paper', getComputerChoice()));
+    scores.textContent = `Player Score: ${playerScore}  Computer Score: ${computerScore}`;
+    winnerResult.textContent = checkScore(playerScore, computerScore);
+   results.append(content, scores, winnerResult);
+
+}) 
+const rock = document.getElementById('rock');
+rock.addEventListener('click', () => {
+    content.textContent = (playGame('rock', getComputerChoice()));
+    scores.textContent = `Player Score: ${playerScore}  Computer Score: ${computerScore}`;
+    winnerResult.textContent = checkScore(playerScore, computerScore);
+   results.append(content, scores, winnerResult);
+}) 
+const scissors = document.getElementById('scissors');
+scissors.addEventListener('click', () => {
+    content.textContent = (playGame('scissors', getComputerChoice()));
+    scores.textContent = `Player Score: ${playerScore}  Computer Score: ${computerScore}`;
+    winnerResult.textContent = checkScore(playerScore, computerScore);
+   results.append(content, scores, winnerResult);
+}) 
+if (playerScore == 5 || computerScore == 5) {
+    console.log("end");
+}
+
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     switch (num) {
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors";
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
     }
 }
 function playGame(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+
     if (playerSelection == computerSelection) {
         playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
         return `Tied! You both chose ${playerSelection}.`;
     } 
     else if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore++;
         return "You Lose!, Paper beats Rock!";
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore++;
         return "You Win!, Rock beats Scissors!";
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerScore++;
         return "You Win!, Paper beats Rock!";
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore++;
         return "You Lose!, Scissors beats Paper!";
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore++;
         return "You Lose!, Rock beats Scissors!";
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore++;
         return "You Win!, Scissors beats Paper!";
     }
     else {
         return "Invalid, Try again!"
     }
 }
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let winner;
-
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, or Scissors?");
-        let message = playGame(playerSelection, getComputerChoice());
-        console.log(message);
-        if (message.slice(0 , 7) == "You Win") {
-            playerScore++;
-        }
-        else if (message.slice(0 , 8) == "You Lose") {
-            computerScore++;
-        }
+function checkScore(PlayerScore, ComputerScore) {
+ if (PlayerScore == 5 || ComputerScore == 5 ) {
+    if (PlayerScore > ComputerScore) {
+        playerScore = 0;
+        computerScore = 0;
+        return "You Win!"
     }
-    if (playerScore > computerScore) {
-        winner = "You!"
+    else if (PlayerScore < ComputerScore)  {
+        playerScore = 0;
+         computerScore = 0;
+        return "Computer Wins!"
     }
-    else if (playerScore < computerScore)
-    {
-        winner = "Computer!"
-    }
-    else {
-        winner = "No Winner, Tie!"
-    }
-    console.log(`Your score: ${playerScore} \nComputer Score: ${computerScore} \nWinner: ${winner}`);
-
+    
+    playerScore = 0;
+    computerScore = 0;
+ }
 }
-game();
